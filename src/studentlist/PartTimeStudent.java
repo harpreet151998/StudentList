176c51ee835c143bc6e578dbceb8fc7ec3809ad0
Harpreet Kaur <kau17129@sheridancollege.ca>
2024-09-17 09:36:12 -0400

    Part time Student added

---
+++ src/studentlist/PartTimeStudent.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package studentlist;
+
+/**
+ *
+ * @author harpr
+ */
+public class PartTimeStudent extends Student{
+    private int numofcourses;
+
+    public int getNumofcourses() {
+        return numofcourses;
+    }
+
+    public void setNumofcourses(int numofcourses) {
+        this.numofcourses = numofcourses;
+    }
+    
+}
